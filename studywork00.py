6ecb9c4045c5cbdf8505765b1391483db726b20c
chognbin <297118275@qq.com>
2022-02-02 21:36:04 +0800

    第二次提交修改代码，增加123

--- studywork00.py
+++ studywork00.py
@@ -5,7 +5,7 @@
 1.坐标（0，0）表示左上角，整个图片类似镶嵌在坐标的第四象限；img.size是一个二位数组，依次为图片的宽和高。
 2.在图片上添加内容时注意对应内容也会占位置，所以不能当作点即pixel来看待，实际处理时需要考虑添加对象的size，这就对应代码中右上角坐标position的获取。
 3.ImageFont的获取有很多方法，其中较简单的方法是从truetype中获取，对应font存放的位置一般是/usr/share/fonts/...
-4.windows下试用相对路径可能会出现资源无法打开的报错，可以试用绝对路径。
+4.windows下试用相对路径可能会出现资源无法打开的报错，可以试用绝对路径。123
 '''
 
 from PIL import Image,ImageDraw,ImageFont
